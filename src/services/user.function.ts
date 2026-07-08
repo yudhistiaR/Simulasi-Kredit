746614912bd7f1bcda3102e92aee3f8e07c98361
Yudhistia <yudhistia110@gmail.com>
2026-07-08 20:07:59 +0800

    fix response status create user fn

--- src/services/user.function.ts
+++ src/services/user.function.ts
@@ -1,13 +1,10 @@
 import { auth } from "#/lib/auth";
 import { createUser } from "#/validator/user";
 import { createServerFn } from "@tanstack/react-start";
-import { getRequestHeaders } from "@tanstack/react-start/server";
 
 export const createUserFn = createServerFn({ method: "POST" })
   .validator(createUser)
   .handler(async ({ data }) => {
-    const headers = getRequestHeaders();
-
     try {
       await auth.api.createUser({
         body: {
@@ -19,14 +16,16 @@ export const createUserFn = createServerFn({ method: "POST" })
             username: data.username,
           },
         },
-        headers,
       });
 
       return {
         success: true,
-        message: "Berhasil membuat penggunak baru",
+        message: "berhasil membuat penggunak baru",
       };
     } catch (error: any) {
-      console.log(error.message || "Internal server error");
+      return {
+        success: false,
+        message: error.message || "Gagal membuat penggunak baru",
+      };
     }
   });
